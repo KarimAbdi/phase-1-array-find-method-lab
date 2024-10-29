2ea799016cb9fa57694d5467cbf1638ea3b596ef
KarimAbdi <abdikarimbenz@gmail.com>
2024-10-29 11:38:36 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowl(record){           
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+}
